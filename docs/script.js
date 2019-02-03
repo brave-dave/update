a3071007036e8276f923d71f97ade98124ae42ac
Your Name <bravegame1234@gmail.com>
2019-02-03 14:59:11 +0400

    v1.00

---
+++ docs/script.js
@@ -0,0 +1,17 @@
+/*const timeBox = document.getElementById("time");
+
+function updateTime() {
+    let date    = new Date();
+    let hours   = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (seconds <= 10) {
+        seconds = "0" + seconds;
+    }
+    var current_date = hours + ":" + minutes + ":" + seconds;
+    timeBox.innerHTML = current_date;
+}
+
+updateTime()
+
+setInterval(function() {updateTime()},1000)*/
